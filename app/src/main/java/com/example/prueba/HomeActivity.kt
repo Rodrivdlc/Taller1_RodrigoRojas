251688b0e7b73e98d7cbf8173213846a0e3c05b9
Rodrivdlc <rodri.vdlc@gmail.com>
2024-09-22 02:02:51 +0200

    homeactivity3

---
+++ app/src/main/java/com/example/prueba/HomeActivity.kt
@@ -0,0 +1,9 @@
+package com.example.prueba
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
