cc83d8b2baa7dac852d17a9b3c4a5dacef32e244
Rodrivdlc <rodri.vdlc@gmail.com>
2024-09-22 02:57:54 +0200

    homeactivity

--- app/src/main/java/com/example/prueba/HomeActivity.kt
+++ app/src/main/java/com/example/prueba/HomeActivity.kt
@@ -1,14 +1,13 @@
 package com.example.prueba
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import java.util.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
